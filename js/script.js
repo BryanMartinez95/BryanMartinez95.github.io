f49b9844ef10e9da5b32af2f3465411ee7859266
werckerbot <pleasemailus@wercker.com>
2016-09-13 03:24:19 +0000

    [ci skip] deploy from Bryan

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-$('li').click(function(){
-     $(this).removeClass('active');
-     $(this).addClass('active');
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    event.stopPropagation();
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+$('html').click(function() {
+  //Hide the menus if visible
+  document.getElementById("mySidenav").style.width = "0";
 });
-console.log("test");
+
+// $('#menucontainer').click(function(event){
+//     event.stopPropagation();
+// });
+
+$(document).ready(function() {
+  $("ul.opMenu li").click(function(){
+   $('#MainOptSubMenu',this).css('visibility', 'visible');
+  });
+
+  $("ul.opMenu li").mouseleave(function(){
+      $('#MainOptSubMenu',this).css('visibility', 'hidden');
+  });
+});
